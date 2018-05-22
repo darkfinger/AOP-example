c6dff25bef98eb477c1f4df93d80c233c7653fd1
darkfinger <892810dkcbak@G>
2018-05-22 09:14:28 -0400

    starter comit

---
+++ src/com/dkkcorp/aopdemo/DemoConfig.java
@@ -0,0 +1,11 @@
+package com.dkkcorp.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.dkkcorp.aopdemo")
+public class DemoConfig {
+}
